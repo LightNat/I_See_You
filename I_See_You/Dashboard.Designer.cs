da733ca1f13f046b8e1bfe477a748a51af090e87
kolp143 <92522193+kolp143@users.noreply.github.com>
2023-05-03 15:28:39 +0800

    sample

--- I_See_You/Dashboard.Designer.cs
+++ I_See_You/Dashboard.Designer.cs
@@ -8,6 +8,7 @@
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
+        /// sample
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
